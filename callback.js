f7a8ebc8ed4e05902f27b8992da7d70d7a7499f2
Mikael Grön <skaramicke@gmail.com>
2023-02-24 11:07:02 +0100

    Make callback return access token instead of script

--- callback.js
+++ callback.js
@@ -1,36 +1,38 @@
-const generateScript = require('./login_script.js')
+const { json } = require("express");
+const generateScript = require("./login_script.js");
 
 module.exports = (oauth2, oauthProvider) => {
-  function callbackMiddleWare (req, res, next) {
-    const code = req.query.code
+  function callbackMiddleWare(req, res, next) {
+    const code = req.query.code;
     var options = {
-      code: code
+      code: code,
+    };
+
+    if (oauthProvider === "gitlab") {
+      options.client_id = process.env.OAUTH_CLIENT_ID;
+      options.client_secret = process.env.OAUTH_CLIENT_SECRET;
+      options.grant_type = "authorization_code";
+      options.redirect_uri = process.env.REDIRECT_URL;
     }
 
-    if (oauthProvider === 'gitlab') {
-      options.client_id = process.env.OAUTH_CLIENT_ID
-      options.client_secret = process.env.OAUTH_CLIENT_SECRET
-      options.grant_type = 'authorization_code'
-      options.redirect_uri = process.env.REDIRECT_URL
-    }
-
-    oauth2.getToken(options)
-      .then(result => {
-        const token = oauth2.createToken(result)
+    oauth2
+      .getToken(options)
+      .then((result) => {
+        const token = oauth2.createToken(result);
         content = {
           token: token.token.token.access_token,
-          provider: oauthProvider
-        }
-        return { message: 'success', content }
+          provider: oauthProvider,
+        };
+        return { message: "success", content };
       })
-      .catch(error => {
-        console.error('Access Token Error', error.message)
-        return { message: 'error', content: JSON.stringify(error) }
+      .catch((error) => {
+        console.error("Access Token Error", error.message);
+        return { message: "error", content: JSON.stringify(error) };
       })
-      .then(result => {
-        const script = generateScript(oauthProvider, result.message, result.content)
-        return res.send(script)
-      })
+      .then((result) => {
+        // const script = generateScript(oauthProvider, result.message, result.content)
+        return res.send(JSON.stringify(result));
+      });
   }
-  return callbackMiddleWare
-}
+  return callbackMiddleWare;
+};
